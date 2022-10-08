ea1f101fc5c50d061e206f373378010a0ef4b723
Mark Vishnevsky <markpenza@gmail.com>
2022-10-09 01:15:01 +0300

    debug print added

--- web/fastapi/parser.py
+++ web/fastapi/parser.py
@@ -4,7 +4,7 @@ def add_link_to_csv(link, roles):
     # link = 'http://www.klerk.ru/blogs/Klerk_invest/537153/'
     # roles = ['Генеральный директор', 'Общее']
     links = get_similar_links(link)
-    
+    print(links)
 
     # TODO:
     # add here some actions
